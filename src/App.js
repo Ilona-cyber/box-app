54697ad24380a68536e37c236659ee7bec22feb5
Ilona <60713890+Ilona-cyber@users.noreply.github.com>
2020-03-19 13:08:13 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Content from './components/Content';
+
+
+
+const App = () => {
+  return (
+  <div className='app-wraper'>
+   <Header/>
+    <Navbar/>
+    <Content/>
+    
+  </div>);
+}
+
+export default App;
